e240c16dc877e988c2b078694b0b56bd0ceb98b2
Sinbad Adjuik <saajuik@gmail.com>
2025-05-27 13:34:54 -0400

    Deleted test api file

--- test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = "751d0352ade25f08a50eab3324d96eec5a8aee04"
-url = "https://fsucardarchive.create.fsu.edu/api/users/current"
-headers = {"Authorization": f"Bearer {api_key}"}
-
-response = requests.get(url, headers=headers, verify=False)
-print("Status Code:", response.status_code)
-print("Response:", response.text)
